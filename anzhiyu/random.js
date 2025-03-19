8431960da6e6fea149fb4509026ad8c38bfcd44b
bygeee <eeee3527424707@163.com>
2025-03-19 23:02:26 +0800

    Site updated: 2025-03-19 23:02:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/08/dumpdex/","2025/03/07/hgame/","2025/03/13/基础/","2025/03/08/ghctf/","2025/03/10/水/"];function toRandomPost(){
+var posts=["2025/03/07/hgame/","2025/03/08/dumpdex/","2025/03/19/smc/","2025/03/08/ghctf/","2025/03/13/基础/","2025/03/10/水/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
