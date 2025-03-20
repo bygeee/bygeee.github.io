a97e27e11c6b8ef509f18db36a2b5f7a9d10f4c4
bygeee <eeee3527424707@163.com>
2025-03-20 19:21:46 +0800

    Site updated: 2025-03-20 19:21:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/08/dumpdex/","2025/03/07/hgame/","2025/03/19/smc/","2025/03/13/基础/","2025/03/13/调试so/","2025/03/10/水/","2025/03/08/ghctf/"];function toRandomPost(){
+var posts=["2025/03/08/dumpdex/","2025/03/08/ghctf/","2025/03/07/hgame/","2025/03/13/基础/","2025/03/10/水/","2025/03/20/调试so/","2025/03/19/smc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
