41e4ae9b8bb3a8fbc89a4100b27e69f191b5fda3
bygeee <eeee3527424707@163.com>
2025-03-24 22:12:18 +0800

    Site updated: 2025-03-24 22:12:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/08/dumpdex/","2025/03/22/frida插桩爆破/","2025/03/08/ghctf/","2025/03/13/基础/","2025/03/19/smc/","2025/03/07/hgame/","2025/03/20/调试so/","2025/03/10/水/","2025/03/23/android so层简单分析/"];function toRandomPost(){
+var posts=["2025/03/08/dumpdex/","2025/03/23/android so层简单分析/","2025/03/08/ghctf/","2025/03/07/hgame/","2025/03/22/frida插桩爆破/","2025/03/19/smc/","2025/03/13/基础/","2025/03/25/nctf/","2025/03/10/水/","2025/03/20/调试so/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
